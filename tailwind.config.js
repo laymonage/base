4b0f528541e3afc07fe5fc78a4b0d941a9870eb1
sage <laymonage@gmail.com>
2021-02-20 21:03:29 +0700

    :wrench: Use Lato font in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Lato', ...fontFamily.sans],
+      },
+    },
+  },
   variants: {
     extend: {
       margin: ['first'],
