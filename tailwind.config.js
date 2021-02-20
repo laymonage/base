70c0b92f34aa99d60c268ffca0afaa23714b4a0b
sage <laymonage@gmail.com>
2021-02-20 16:35:07 +0700

    :wrench: Add first selector to Tailwind margin variant

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
+  variants: {
+    extend: {
+      margin: ['first'],
+    },
+  },
 };
