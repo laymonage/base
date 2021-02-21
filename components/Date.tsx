30656ad6a9708ed0b4f4ada7e1e8b5856023e76b
sage <laymonage@gmail.com>
2021-02-21 23:45:43 +0700

    :sparkles: Accept className in Date component

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,12 +1,19 @@
 import { parseISO, format } from 'date-fns';
 import PropTypes from 'prop-types';
 
-export const propTypes = { dateString: PropTypes.string.isRequired };
-export type DateString = PropTypes.InferProps<typeof propTypes>;
+export const propTypes = {
+  dateString: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+export type DateProps = PropTypes.InferProps<typeof propTypes>;
 
-const Date: React.FC<DateString> = ({ dateString }) => {
+const Date: React.FC<DateProps> = ({ dateString, className }) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>;
+  return (
+    <time dateTime={dateString} className={className}>
+      {format(date, 'LLLL d, yyyy')}
+    </time>
+  );
 };
 Date.propTypes = propTypes;
 export default Date;
