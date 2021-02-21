f190d924af52292b75593eef9c5f0dd9e3240f68
sage <laymonage@gmail.com>
2021-02-21 23:01:29 +0700

    :lipstick: Render Card header and subtitle conditionally

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,10 +10,12 @@ export type CardProps = PropTypes.InferProps<typeof propTypes>;
 const Card: React.FC<CardProps> = ({ children, header, subtitle }) => {
   return (
     <div className="flex-col w-full p-6 transition-colors duration-500 bg-white rounded shadow-md sm:p-8 dark:bg-gray-800 ease">
-      <div className="flex flex-col items-baseline text-4xl text-blue-800 mb-14 dark:text-blue-200 md:text-5xl sm:flex-row">
-        {header}
-        <div className="text-2xl md:text-3xl">{subtitle}</div>
-      </div>
+      {header && (
+        <div className="flex flex-col items-baseline text-4xl text-blue-800 mb-14 dark:text-blue-200 md:text-5xl sm:flex-row">
+          {header}
+          {subtitle && <div className="text-2xl md:text-3xl">{subtitle}</div>}
+        </div>
+      )}
       {children}
     </div>
   );
