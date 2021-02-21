1e690361dc8fda8f20eb0165217a2a374fdaac4d
sage <laymonage@gmail.com>
2021-02-21 23:00:21 +0700

    :construction: Use JSON schema for parsing YAML frontmatter

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,5 +1,14 @@
+import gm from 'gray-matter';
+import yaml from 'js-yaml';
 import remark from 'remark';
 import html from 'remark-html';
 
 export const md = async (markup: string): Promise<string> =>
   (await remark().use(html).process(markup)).toString();
+
+export const matter = (fileContent: string): gm.GrayMatterFile<string> =>
+  gm(fileContent, {
+    engines: {
+      yaml: (s) => yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA }) as Record<string, unknown>,
+    },
+  });
