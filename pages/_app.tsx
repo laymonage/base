f96b538d4fe551e0f1a185395b582e0d91f46988
sage <laymonage@gmail.com>
2021-02-21 07:32:41 +0700

    :construction: Use ThemeProvider in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default App;
