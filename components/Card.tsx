290aa20102a69e94065daaf5dd38d62795f1afdd
sage <laymonage@gmail.com>
2021-02-22 15:27:22 +0700

    :lipstick: Reduce header margin-bottom in Card

--- components/Card.tsx
+++ components/Card.tsx
@@ -11,7 +11,7 @@ const Card: React.FC<CardProps> = ({ children, header, subtitle }) => {
   return (
     <div className="flex-col w-full p-6 transition-colors duration-500 bg-white rounded shadow-md sm:p-8 dark:bg-gray-800 ease">
       {header && (
-        <div className="flex flex-col items-baseline text-4xl text-blue-800 mb-14 dark:text-blue-200 md:text-5xl sm:flex-row">
+        <div className="flex flex-col items-baseline mb-8 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row">
           {header}
           {subtitle && <div className="text-2xl md:text-3xl">{subtitle}</div>}
         </div>
