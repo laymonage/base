4cb4a61bedcee7cdea194a71a104d29f28f6df9d
sage <laymonage@gmail.com>
2021-02-22 12:13:13 +0700

    :sparkles: Create posts/[slug] page

---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,60 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import PropTypes from 'prop-types';
+import Card from '../../components/Card';
+import Date from '../../components/Date';
+import Layout from '../../components/Layout';
+import { getAllPostSlugs, getPostData } from '../../lib/content';
+import { postPropTypes } from '../../lib/models/content';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostSlugs();
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const post = await getPostData(params.slug as string);
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+const propTypes = { post: PropTypes.exact(postPropTypes) };
+type PostProps = PropTypes.InferProps<typeof propTypes>;
+
+const SinglePost: React.FC<PostProps> = ({ post }) => {
+  return (
+    <div className="w-full mx-auto mt-2 mb-16 sm:mt-32">
+      <Layout>
+        <div className="mx-auto mt-16 mb-2 first:mt-0 post">
+          <Card>
+            <div className="flex-row text-center">
+              <h2 className="mb-4 text-3xl font-bold">{post.data.title}</h2>
+              <Date className="block mb-4" dateString={post.data.date} />
+              <div className="mb-16">
+                {post.data.tags.map((tag) => (
+                  <span
+                    className="p-1 mx-1 bg-gray-400 border-2 border-gray-500 rounded bg-opacity-20"
+                    key={tag}
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div
+              className="mt-4 sm:mx-4 markdown max-w-prose"
+              dangerouslySetInnerHTML={{ __html: post.content }}
+            ></div>
+          </Card>
+        </div>
+      </Layout>
+    </div>
+  );
+};
+SinglePost.propTypes = propTypes;
+export default SinglePost;
