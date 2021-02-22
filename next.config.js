f26571ea63f43d13bd98f1393a508ecaa805a57c
sage <laymonage@gmail.com>
2021-02-22 22:20:29 +0700

    :zap: Use preact in client production build

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
+};
