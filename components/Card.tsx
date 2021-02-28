cc559e07df110867aa258545fdb0dfe39f460e4b
sage <laymonage@gmail.com>
2021-02-28 08:10:52 +0700

    :lipstick: Only use flex for header when subtitle is available

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import cn from 'classnames';
 
 export interface CardProps {
   children: ReactNode;
@@ -10,7 +11,11 @@ const Card = ({ children, header, subtitle }: CardProps) => {
   return (
     <div className="flex-col w-full p-6 bg-white rounded shadow-md sm:p-8 dark:bg-gray-800">
       {header && (
-        <div className="flex flex-col items-baseline mb-8 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row">
+        <div
+          className={cn('mb-8 text-4xl text-blue-800 dark:text-blue-200 md:text-5xl sm:flex-row', {
+            'flex flex-col items-baseline': !!(header && subtitle),
+          })}
+        >
           {header}
           {subtitle && <div className="text-2xl md:text-3xl">{subtitle}</div>}
         </div>
