fd81bbcbca9ec17b3082f3e19625b2e7914f5da2
sage <laymonage@gmail.com>
2021-03-06 19:26:23 +0700

    :lipstick: Set maximum Navigation width to 3xl

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -21,41 +21,42 @@ const Navigation = () => {
 
   const menu = ['posts', 'projects', 'about'];
   return (
-    <nav
-      role="navigation"
-      className={cn(
-        'fixed bottom-0 z-20 flex flex-col-reverse w-full py-3 transition-transform duration-500 transform bg-white sm:flex-row sm:justify-between sm:items-center dark:bg-gray-800 dark:text-blue-200 ease sm:top-0 sm:bottom-auto',
-        { 'shadow-md': !hidden, 'translate-y-full': hidden, 'sm:-translate-y-full': hidden },
-      )}
-    >
-      <div className="flex items-center justify-between flex-grow mx-4">
-        <Link href="/" aria-current aria-label="Home">
-          <a
-            tabIndex={-1}
-            className="w-10 h-10 text-blue-700 fill-current focus:outline-none dark:text-blue-100"
-          >
-            <Logo />
-          </a>
-        </Link>
-        <div className="flex items-center justify-between text-blue-700 dark:text-blue-200">
-          <ThemeToggle />
-          <button
-            type="button"
-            className="w-8 h-8 ml-4 fill-current sm:hidden focus:outline-none focus:text-blue-600 hover:text-blue-600 dark:focus:text-blue-100 dark:hover:text-blue-100"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <Times /> : <Bars />}
-          </button>
-        </div>
-      </div>
-      <div className={cn('px-2 pb-2 sm:p-0 sm:mr-4 sm:flex sm:items-center', { hidden: !open })}>
-        {menu.map((m, i) => (
-          <Link href={`/${m}`} key={i}>
-            <a className="block p-2 mb-2 font-bold capitalize rounded sm:mb-0 first:mb-1 sm:first:mb-0 sm:ml-4 sm:first:ml-0 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 dark:focus:bg-gray-700 dark:hover:bg-gray-700">
-              {m}
+    <nav role="navigation" className="max-w-3xl mx-auto">
+      <div
+        className={cn(
+          'fixed bottom-0 z-20 flex flex-col-reverse w-full max-w-3xl py-3 transition-transform duration-500 transform bg-white sm:flex-row sm:justify-between sm:items-center dark:bg-gray-800 dark:text-blue-200 ease sm:top-0 sm:bottom-auto',
+          { 'shadow-md': !hidden, 'translate-y-full': hidden, 'sm:-translate-y-full': hidden },
+        )}
+      >
+        <div className="flex items-center justify-between flex-grow mx-4">
+          <Link href="/" aria-current aria-label="Home">
+            <a
+              tabIndex={-1}
+              className="w-10 h-10 text-blue-700 fill-current focus:outline-none dark:text-blue-100"
+            >
+              <Logo />
             </a>
           </Link>
-        ))}
+          <div className="flex items-center justify-between text-blue-700 dark:text-blue-200">
+            <ThemeToggle />
+            <button
+              type="button"
+              className="w-8 h-8 ml-4 fill-current sm:hidden focus:outline-none focus:text-blue-600 hover:text-blue-600 dark:focus:text-blue-100 dark:hover:text-blue-100"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <Times /> : <Bars />}
+            </button>
+          </div>
+        </div>
+        <div className={cn('px-2 pb-2 sm:p-0 sm:mr-4 sm:flex sm:items-center', { hidden: !open })}>
+          {menu.map((m, i) => (
+            <Link href={`/${m}`} key={i}>
+              <a className="block p-2 mb-2 font-bold capitalize rounded sm:mb-0 first:mb-1 sm:first:mb-0 sm:ml-4 sm:first:ml-0 focus:outline-none focus:bg-blue-100 hover:bg-blue-100 dark:focus:bg-gray-700 dark:hover:bg-gray-700">
+                {m}
+              </a>
+            </Link>
+          ))}
+        </div>
       </div>
     </nav>
   );
