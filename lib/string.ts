531033775fe24ceecacaeebe33115b9db7db604e
sage <laymonage@gmail.com>
2021-03-11 09:22:09 +0700

    :construction: Create parseLogSlug function

--- lib/string.ts
+++ lib/string.ts
@@ -1,6 +1,11 @@
 export const capitalize = (s: string): string => s.charAt(0).toUpperCase() + s.slice(1);
 
-export const humanizeLogSlug = (slug: string) => {
+export const parseLogSlug = (slug: string) => {
   const [year, week] = slug.split('w');
-  return `20${year} Week ${week}`;
+  return { year: `20${year}`, week };
+};
+
+export const humanizeLogSlug = (slug: string) => {
+  const { year, week } = parseLogSlug(slug);
+  return `${year} Week ${week}`;
 };
