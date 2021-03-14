fece381cb5c867d47281a8c8de57e3d765586a48
sage <laymonage@gmail.com>
2021-03-14 07:38:12 +0700

    :construction: Create adapter function for CurrentlyPlaying

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -1,4 +1,4 @@
-import { RefreshTokenResponse } from './models/spotify';
+import { CurrentlyPlaying, NowPlaying, RefreshTokenResponse } from './models/spotify';
 
 const SPOTIFY_API_BASE_URL = 'https://api.spotify.com/v1';
 const SPOTIFY_ACCOUNTS_BASE_URL = 'https://accounts.spotify.com';
@@ -54,3 +54,14 @@ const fetchWith = {
 export const getNowPlaying = async () => fetchWith.accessToken(ENDPOINTS.NOW_PLAYING);
 
 export const getTopTracks = async () => fetchWith.accessToken(ENDPOINTS.TOP_TRACKS);
+
+// Adapter functions
+
+export const adaptNowPlaying = (track: CurrentlyPlaying): NowPlaying => ({
+  isPlaying: track.is_playing,
+  title: track.item.name,
+  artist: track.item.artists.map((artist) => artist.name).join(', '),
+  album: track.item.album.name,
+  albumImageUrl: track.item.album.images[0]?.url,
+  trackUrl: track.item.external_urls.spotify,
+});
