f7d7650cb5397c9df336a8085e5b5a753869562f
sage <laymonage@gmail.com>
2021-03-14 07:40:09 +0700

    :construction: Create now-playing API

---
+++ pages/api/now-playing.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { adaptNowPlaying, getNowPlaying } from 'lib/spotify';
+import { CurrentlyPlaying } from 'lib/models/spotify';
+
+export default async (_: NextApiRequest, res: NextApiResponse) => {
+  const response = await getNowPlaying();
+
+  if (response.status !== 200) {
+    return res.status(200).json({ isPlaying: false });
+  }
+
+  const track: CurrentlyPlaying = await response.json();
+  const nowPlaying = adaptNowPlaying(track);
+
+  res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate=30');
+
+  return res.status(200).json(nowPlaying);
+};
