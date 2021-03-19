5fb1bc71272f39cc08e93373d88f07b4c382890a
sage <laymonage@gmail.com>
2021-03-19 11:50:38 +0700

    :truck: Rename globals.css to globals.scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css';
-import 'styles/globals.css';
+import 'styles/globals.scss';
 import 'styles/markdown.scss';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
