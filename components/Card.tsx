9c8a513a148f23175f64472517c407e4dea81cb0
sage <laymonage@gmail.com>
2021-05-15 13:57:07 +0700

    :recycle: Replace boolean operator short-circuits with ternaries

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,16 +10,16 @@ export interface CardProps {
 const Card = ({ children, header, subtitle }: CardProps) => {
   return (
     <div className="flex-col w-full p-8 bg-white rounded shadow-md dark:bg-gray-800">
-      {header && (
+      {header ? (
         <div
           className={cn('mb-8 text-4xl text-blue-800 dark:text-blue-200 sm:text-5xl sm:flex-row', {
             'flex flex-col items-baseline': !!(header && subtitle),
           })}
         >
           {header}
-          {subtitle && <div className="text-2xl sm:text-3xl">{subtitle}</div>}
+          {subtitle ? <div className="text-2xl sm:text-3xl">{subtitle}</div> : null}
         </div>
-      )}
+      ) : null}
       {children}
     </div>
   );
--- components/CatalogItem.tsx
+++ components/CatalogItem.tsx
@@ -16,7 +16,7 @@ export interface Item {
 const CatalogItem = (item: Item) => {
   return (
     <div className="flex flex-col items-center sm:flex-row">
-      {item.image && (
+      {item.image ? (
         <div className="w-9/12 p-2 mb-6 mr-0 rounded sm:w-5/12 sm:mb-0 sm:mr-8">
           <div className="relative h-48 sm:h-36">
             <Image
@@ -29,7 +29,7 @@ const CatalogItem = (item: Item) => {
             />
           </div>
         </div>
-      )}
+      ) : null}
       <div className="flex flex-col justify-between w-full">
         <h3 className="text-2xl">
           <a href={item.url} className="dark:text-blue-200" rel="noreferrer noopener nofollow">
