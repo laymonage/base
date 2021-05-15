0d37c401c5eb81a93582a13d257ccf1a3d6f4c89
sage <laymonage@gmail.com>
2021-05-15 13:50:37 +0700

    :recycle: Extract useMounted hook

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,24 +1,21 @@
+import { useMounted } from 'lib/hooks/mounted';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
 import Moon from './icons/Moon.svg';
 import Sun from './icons/Sun.svg';
 
 const ThemeToggle = () => {
-  const [mounted, setMounted] = useState<boolean>(null);
+  const mounted = useMounted();
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => setMounted(true), []);
-  return (
-    mounted && (
-      <button
-        type="button"
-        className="w-6 h-6 fill-current focus:outline-none alike"
-        aria-label="Theme"
-        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-      >
-        {theme === 'dark' ? <Moon /> : <Sun />}
-      </button>
-    )
-  );
+  return mounted ? (
+    <button
+      type="button"
+      className="w-6 h-6 fill-current focus:outline-none alike"
+      aria-label="Theme"
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+    >
+      {theme === 'dark' ? <Moon /> : <Sun />}
+    </button>
+  ) : null;
 };
 export default ThemeToggle;
