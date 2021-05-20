d8b4aea8a0c0842532680bb2ebdf0a87ae37a5b5
sage <laymonage@gmail.com>
2021-05-20 21:07:09 +0700

    :zap: Add useCallback to theme toggle handler

--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -1,5 +1,6 @@
 import { useMounted } from 'lib/hooks/mounted';
 import { useTheme } from 'next-themes';
+import { useCallback } from 'react';
 import Moon from './icons/Moon.svg';
 import Sun from './icons/Sun.svg';
 
@@ -7,12 +8,17 @@ const ThemeToggle = () => {
   const mounted = useMounted();
   const { resolvedTheme: theme, setTheme } = useTheme();
 
+  const handleThemeChange = useCallback(
+    () => setTheme(theme === 'dark' ? 'light' : 'dark'),
+    [theme],
+  );
+
   return mounted ? (
     <button
       type="button"
       className="w-6 h-6 fill-current focus:outline-none alike"
       aria-label="Theme"
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={handleThemeChange}
     >
       {theme === 'dark' ? <Moon /> : <Sun />}
     </button>
