e984b77408fe8df5a6fe1f17b107c94a7df12b97
sage <laymonage@gmail.com>
2021-05-29 18:27:24 +0700

    :lipstick: Temporarily hide post tags

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -27,6 +27,7 @@ interface PostProps {
 }
 
 const SinglePost = ({ post }: PostProps) => {
+  const showTags = false;
   return (
     <Layout
       customMeta={{
@@ -40,14 +41,16 @@ const SinglePost = ({ post }: PostProps) => {
           <h2 className="mb-4 text-3xl font-bold">{post.data.title}</h2>
           <Date className="block mb-4" dateString={post.data.date} />
           <div className="mb-16">
-            {post.data.tags.map((tag) => (
-              <span
-                className="p-1 mx-1 bg-gray-400 border-2 border-gray-500 rounded bg-opacity-20"
-                key={tag}
-              >
-                {tag}
-              </span>
-            ))}
+            {showTags
+              ? post.data.tags.map((tag) => (
+                  <span
+                    className="p-1 mx-1 bg-gray-400 border-2 border-gray-500 rounded bg-opacity-20"
+                    key={tag}
+                  >
+                    {tag}
+                  </span>
+                ))
+              : null}
           </div>
         </div>
         <div
