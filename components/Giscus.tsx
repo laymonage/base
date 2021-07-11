f900b6e7a8093186fd924845318c72a8de5d2ba9
sage <laymonage@gmail.com>
2021-07-11 23:22:47 +0700

    :zap: Use setConfig message to update giscus theme

--- components/Giscus.tsx
+++ components/Giscus.tsx
@@ -1,32 +1,49 @@
-import Head from 'next/head';
-import { useMounted } from 'lib/hooks/mounted';
 import { useTheme } from 'next-themes';
+import { useEffect, useRef } from 'react';
 
 const themeMapping = {
   light: 'light',
   dark: 'transparent_dark',
-  system: 'preferred_color_scheme',
 };
 
 export default function Giscus() {
-  const mounted = useMounted();
-  const { theme = 'light' } = useTheme();
+  const { resolvedTheme = 'light' } = useTheme();
+  const theme = useRef(resolvedTheme);
 
-  return !mounted ? null : (
-    <>
-      <Head>
-        <script
-          src="https://giscus.app/client.js"
-          data-repo="laymonage/base"
-          data-repo-id="MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY="
-          data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4"
-          data-mapping="pathname"
-          data-theme={themeMapping[theme as keyof typeof themeMapping]}
-          crossOrigin="anonymous"
-          async
-        />
-      </Head>
-      <div className="giscus" />
-    </>
-  );
+  useEffect(() => {
+    const script = document.createElement('script');
+    const attributes = {
+      src: 'https://giscus.app/client.js',
+      id: 'giscus-script',
+      'data-repo': 'laymonage/base',
+      'data-repo-id': 'MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY',
+      'data-category-id': 'MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4',
+      'data-mapping': 'pathname',
+      'data-theme': themeMapping[theme.current as keyof typeof themeMapping],
+      crossOrigin: 'anonymous',
+      async: '',
+    };
+    Object.entries(attributes).forEach(([name, value]) => script.setAttribute(name, value));
+    document.body.appendChild(script);
+    return () => {
+      const existingScript = document.body.querySelector('#giscus-script');
+      if (existingScript) document.body.removeChild(existingScript);
+    };
+  }, []);
+
+  useEffect(() => {
+    const iframe = document.querySelector<HTMLIFrameElement>('iframe.giscus-frame');
+    iframe?.contentWindow?.postMessage(
+      {
+        giscus: {
+          setConfig: {
+            theme: themeMapping[resolvedTheme as keyof typeof themeMapping],
+          },
+        },
+      },
+      'https://giscus.app',
+    );
+  }, [resolvedTheme]);
+
+  return <div className="giscus" />;
 }
