2a69752fb7ba4bfec57a80cffacd5225940280d8
sage <laymonage@gmail.com>
2021-08-06 19:16:02 +0700

    :lipstick: Revamp footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-start justify-center w-full max-w-3xl p-4 mx-auto">
       <hr className="w-full mb-8 border-gray-400 border-1 border-opacity-20" />
-      <div className="grid w-full max-w-2xl grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+      <div className="grid justify-between w-full max-w-3xl gap-4 grid-cols-2 pb-16 sm:grid-cols-[repeat(3,min-content)]">
         <div className="flex flex-col space-y-4">
           <Link href="/">
             <a>Home</a>
@@ -17,15 +17,25 @@ export default function Footer() {
           <Link href="/posts">
             <a>Posts</a>
           </Link>
-          <Link href="/projects">
-            <a>Projects</a>
+          <Link href="/logs">
+            <a>Logs</a>
           </Link>
         </div>
         <div className="flex flex-col space-y-4">
+          <Link href="/projects">
+            <a>Projects</a>
+          </Link>
           <Link href="/guestbook">
             <a>Guestbook</a>
           </Link>
         </div>
+      </div>
+      <div className="w-full text-sm text-center">
+        © 2021 – made with{' '}
+        <span className="duration-700 cursor-pointer hover:text-red-500 transition-color">
+          ♥
+        </span>{' '}
+        by laymonage
       </div>
     </footer>
   );
