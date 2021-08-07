274e5b0d50093381d7231de290990d360d290ff3
sage <laymonage@gmail.com>
2021-08-07 09:51:11 +0700

    :wrench: Add ignore patterns for no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,10 @@ module.exports = {
       extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^(_)+', varsIgnorePattern: '^(_)+' },
+        ],
       },
     },
   ],
