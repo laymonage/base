48ce8feeee8a2f3bf85be1b27cb6471a78fbd58d
sage <laymonage@gmail.com>
2021-08-07 10:10:09 +0700

    :recycle: Extract gray-matter config

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -7,13 +7,11 @@ import html from 'remark-html';
 export const md = async (markup: string): Promise<string> =>
   (await remark().use(gfm).use(html).process(markup)).toString();
 
+export const grayMatterEngines = {
+  yaml: (s: string) =>
+    // Prevent Date parsing
+    yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA }) as Record<string, unknown>,
+};
+
 export const matter = (fileContent: string): gm.GrayMatterFile<string> =>
-  gm(fileContent, {
-    engines: {
-      yaml: (s) =>
-        yaml.safeLoad(s, { schema: yaml.JSON_SCHEMA }) as Record<
-          string,
-          unknown
-        >,
-    },
-  });
+  gm(fileContent, { engines: grayMatterEngines });
