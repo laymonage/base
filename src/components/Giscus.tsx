880bf23e0d76ea629188d7b26be874cbb7142114
Sage Abdullah <me@laymonage.com>
2022-05-14 10:55:18 +0700

    :arrow_up: Replace giscus with @giscus/react

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'next-themes';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
+import GiscusComponent from '@giscus/react';
 
 const themeMapping = {
   light: 'light',
@@ -9,15 +9,7 @@ const themeMapping = {
 
 export default function Giscus() {
   const { resolvedTheme } = useTheme();
-  const [isMounted, setIsMounted] = useState(false);
   const theme = themeMapping[resolvedTheme as keyof typeof themeMapping];
-
-  useEffect(() => {
-    import('giscus');
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) return null;
 
   return (
     <>
@@ -33,16 +25,16 @@ export default function Giscus() {
           />
         ))}
       </Head>
-      <giscus-widget
+      <GiscusComponent
         repo="laymonage/base"
-        repoid="MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY"
+        repoId="MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY"
         category="Comments"
-        categoryid="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4"
+        categoryId="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4"
         theme={theme}
         mapping="pathname"
-        reactionsenabled="1"
-        emitmetadata="0"
-        inputposition="bottom"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="bottom"
         lang="en"
       />
     </>
