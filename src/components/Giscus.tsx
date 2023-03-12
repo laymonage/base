447cc46f7d583de9750f26b964585dc1a8261387
Sage Abdullah <me@laymonage.com>
2023-03-12 16:45:26 +0000

    :zap: Allow Giscus to be loaded lazily

--- src/components/Giscus.tsx
+++ src/components/Giscus.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTheme } from 'next-themes';
 import Head from 'next/head';
 import GiscusComponent from '@giscus/react';
@@ -7,8 +8,13 @@ const themeMapping = {
   dark: 'transparent_dark',
 };
 
-export default function Giscus() {
+interface GiscusProps {
+  eager?: boolean;
+}
+
+export default function Giscus({ eager = false }: GiscusProps) {
   const { resolvedTheme } = useTheme();
+  const [shown, setShown] = useState(eager);
   const theme = themeMapping[resolvedTheme as keyof typeof themeMapping];
 
   return (
@@ -25,18 +31,28 @@ export default function Giscus() {
           />
         ))}
       </Head>
-      <GiscusComponent
-        repo="laymonage/base"
-        repoId="MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY"
-        category="Comments"
-        categoryId="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4"
-        theme={theme}
-        mapping="pathname"
-        reactionsEnabled="1"
-        emitMetadata="0"
-        inputPosition="bottom"
-        lang="en"
-      />
+      {shown ? (
+        <GiscusComponent
+          repo="laymonage/base"
+          repoId="MDEwOlJlcG9zaXRvcnkzNDExNDE2OTY"
+          category="Comments"
+          categoryId="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMyNzIzMDI4"
+          theme={theme}
+          mapping="pathname"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          inputPosition="bottom"
+          lang="en"
+        />
+      ) : (
+        <button
+          type="button"
+          className="hover rounded bg-gray-100 py-2 px-4 text-center text-base text-gray-600 hover:bg-gray-200  hover:text-gray-800 focus:bg-gray-200 focus:text-gray-800 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:bg-gray-700 dark:focus:text-white"
+          onClick={() => setShown(true)}
+        >
+          Load comments
+        </button>
+      )}
     </>
   );
 }
