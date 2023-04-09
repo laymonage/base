f19413152b96a458cb98ee7cfd3ad4997fd634f9
Sage Abdullah <me@laymonage.com>
2023-04-09 14:56:51 +0100

    💄 Add loading animation to saved tracks table

--- src/components/SavedTracksTable.tsx
+++ src/components/SavedTracksTable.tsx
@@ -219,98 +219,110 @@ export default function SavedTracksTable({
       <div
         ref={tableContainerRef}
         className={clsx(
-          'h-[max(calc(100vh-10rem),16rem)] overflow-auto',
+          'h-[max(calc(100vh-14rem),16rem)] overflow-auto',
           className,
         )}
       >
-        <table className="w-full min-w-max table-fixed border-collapse whitespace-nowrap">
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th
-                    className={clsx(
-                      getColumnMeta(header.column.columnDef)?.class,
-                      'p-2',
-                    )}
-                    key={header.id}
-                  >
-                    {header.isPlaceholder ? null : (
-                      <div
-                        className={clsx({
-                          'cursor-pointer select-none':
-                            header.column.getCanSort(),
-                        })}
-                        onClick={header.column.getToggleSortingHandler()}
-                      >
-                        {flexRender(
-                          header.column.columnDef.header,
-                          header.getContext(),
-                        )}
-                        {{
-                          asc: ' 🔼',
-                          desc: ' 🔽',
-                        }[header.column.getIsSorted() as string] ?? null}
-                      </div>
-                    )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody>
-            {paddingTop > 0 ? (
-              <tr>
-                <td style={{ height: `${paddingTop}px` }} />
-              </tr>
-            ) : null}
-            {virtualRows.map((virtualRow) => {
-              const row = rows[virtualRow.index] as Row<SavedTrackSimplified>;
-              return (
-                <tr
-                  key={row.original.id}
-                  className="group/row focus-within:bg-blue-300 focus-within:bg-opacity-10 hover:bg-blue-300 hover:bg-opacity-10"
-                >
-                  {row.getVisibleCells().map((cell) => (
-                    <td
+        {!fetched ? (
+          <div className="flex h-full flex-col items-center justify-center text-center">
+            <img
+              aria-hidden="true"
+              className="mx-auto my-2"
+              alt="Loading…"
+              src="/img/equaliser-animated-green.gif"
+            />
+            Loading…
+          </div>
+        ) : (
+          <table className="w-full min-w-max table-fixed border-collapse whitespace-nowrap">
+            <thead>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th
                       className={clsx(
-                        getColumnMeta(cell.column.columnDef)?.class,
-                        'p-2 first-of-type:rounded-l last-of-type:rounded-r',
+                        getColumnMeta(header.column.columnDef)?.class,
+                        'p-2',
                       )}
-                      key={cell.id}
+                      key={header.id}
                     >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
+                      {header.isPlaceholder ? null : (
+                        <div
+                          className={clsx({
+                            'cursor-pointer select-none':
+                              header.column.getCanSort(),
+                          })}
+                          onClick={header.column.getToggleSortingHandler()}
+                        >
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext(),
+                          )}
+                          {{
+                            asc: ' 🔼',
+                            desc: ' 🔽',
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </div>
                       )}
-                    </td>
+                    </th>
                   ))}
                 </tr>
-              );
-            })}
-            {paddingBottom > 0 ? (
-              <tr>
-                <td style={{ height: `${paddingBottom}px` }} />
-              </tr>
-            ) : null}
-          </tbody>
-          <tfoot>
-            {table.getFooterGroups().map((footerGroup) => (
-              <tr key={footerGroup.id}>
-                {footerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.footer,
-                          header.getContext(),
+              ))}
+            </thead>
+            <tbody>
+              {paddingTop > 0 ? (
+                <tr>
+                  <td style={{ height: `${paddingTop}px` }} />
+                </tr>
+              ) : null}
+              {virtualRows.map((virtualRow) => {
+                const row = rows[virtualRow.index] as Row<SavedTrackSimplified>;
+                return (
+                  <tr
+                    key={row.original.id}
+                    className="group/row focus-within:bg-blue-300 focus-within:bg-opacity-10 hover:bg-blue-300 hover:bg-opacity-10"
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <td
+                        className={clsx(
+                          getColumnMeta(cell.column.columnDef)?.class,
+                          'p-2 first-of-type:rounded-l last-of-type:rounded-r',
                         )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </tfoot>
-        </table>
+                        key={cell.id}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </td>
+                    ))}
+                  </tr>
+                );
+              })}
+              {paddingBottom > 0 ? (
+                <tr>
+                  <td style={{ height: `${paddingBottom}px` }} />
+                </tr>
+              ) : null}
+            </tbody>
+            <tfoot>
+              {table.getFooterGroups().map((footerGroup) => (
+                <tr key={footerGroup.id}>
+                  {footerGroup.headers.map((header) => (
+                    <th key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.footer,
+                            header.getContext(),
+                          )}
+                    </th>
+                  ))}
+                </tr>
+              ))}
+            </tfoot>
+          </table>
+        )}
       </div>
     </AudioProvider>
   );
