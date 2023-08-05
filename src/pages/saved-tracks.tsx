e9b76632f275860cd87be0c7e0a895723a31fa4e
Sage Abdullah <me@laymonage.com>
2023-08-05 23:26:13 +0100

    ✨ Add link to Liked Songs (mirror) playlist and use Spotify's cover image

--- src/pages/saved-tracks.tsx
+++ src/pages/saved-tracks.tsx
@@ -4,7 +4,6 @@ import Link from '@/components/Link';
 import SpotifyTracksTable from '@/components/SpotifyTracksTable';
 import { useData } from '@/lib/hooks/data';
 import { ComponentProps } from 'react';
-import { Heart } from 'react-feather';
 
 const SPOTIFY_SAVED_TRACKS_RAW_URL =
   'https://raw.githubusercontent.com/laymonage/spotify-to-github';
@@ -25,7 +24,10 @@ export default function Selections() {
       <div className="bleed w-full max-w-4xl place-self-center">
         <div className="my-4 mb-14 flex flex-wrap items-end gap-8 sm:flex-nowrap">
           <div className="flex h-48 w-48 flex-shrink-0 items-center justify-center bg-blue-200 bg-opacity-50">
-            <Heart fill="currentColor" className="h-20 w-20" />
+            <img
+              alt=""
+              src="https://t.scdn.co/images/3099b3803ad9496896c43f22fe9be8c4.png"
+            />
           </div>
           <Card header="Saved tracks">
             <p>
@@ -35,7 +37,11 @@ export default function Selections() {
               </Link>{' '}
               that exports my Spotify saved tracks every day. It allows me to
               share the playlist here, without having to make a Spotify API call
-              every time someone visits this page.
+              every time someone visits this page. You can also{' '}
+              <Link href="https://open.spotify.com/playlist/6T5QnaTXvu6ckKwcxANEwp">
+                follow the playlist on Spotify
+              </Link>
+              .
             </p>
           </Card>
         </div>
