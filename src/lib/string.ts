a15e6cc038d19a952ead1f53c492f930678e0c43
Sage Abdullah <me@laymonage.com>
2023-08-19 13:12:07 +0100

    🐛 No-op decodeHTML when called in Node.js

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -12,6 +12,7 @@ export const humanizeLogSlug = (slug: string) => {
 };
 
 export const decodeHTML = (html: string) => {
+  if (typeof document === 'undefined') return html;
   const text = document.createElement('textarea');
   text.innerHTML = html;
   return text.value;
