8ac27beb1d9df3c2cb626b73746c040c698a9a5e
Sage Abdullah <me@laymonage.com>
2024-10-13 22:51:52 +0100

    Migrate homepage from base (initial)

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'laymonage';
+export const SITE_DESCRIPTION =
+  'laymonage is Sage Abdullah. I build things up and break them down in the open.';
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Source Sans Pro"', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
