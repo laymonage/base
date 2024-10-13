2a716cefee713116aa0bec915aa5017c8c6d7d11
Sage Abdullah <me@laymonage.com>
2024-10-13 11:34:06 +0100

    Add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
 });
