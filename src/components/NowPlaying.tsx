723d7574ce84d7f92e59c43a0a15091356a63bd3
Sage Abdullah <me@laymonage.com>
2024-10-16 22:54:46 +0100

    Add NowPlaying component to about page

---
+++ src/components/NowPlaying.tsx
@@ -0,0 +1,41 @@
+import clsx from 'clsx';
+import useSWR from 'swr';
+import { fetcher } from '../lib/fetcher';
+import type { NowPlaying } from '../lib/spotify';
+
+const SPOTIFY_PROFILE_URL = 'https://open.spotify.com/user/laymonage';
+
+export default function NowPlaying({ icon }: { icon?: JSX.Element }) {
+  const { data } = useSWR<NowPlaying>('/api/now-playing', fetcher);
+  const isPlaying = data?.isPlaying;
+
+  return (
+    <a
+      className="ml-auto flex min-h-[56px] max-w-full items-center"
+      href={isPlaying ? data?.trackUrl : SPOTIFY_PROFILE_URL}
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+    >
+      <div className="mr-4 flex flex-col truncate text-right">
+        {data && isPlaying ? (
+          <>
+            <p className="truncate">{data.artist}</p>
+            <p className="truncate">{data.title}</p>
+          </>
+        ) : (
+          <p>Not playing</p>
+        )}
+      </div>
+      {icon ? (
+        <div
+          className={clsx('flex-shrink-0', {
+            'h-8 w-8': isPlaying,
+            'h-6 w-6': !isPlaying,
+          })}
+        >
+          {icon}
+        </div>
+      ) : null}
+    </a>
+  );
+}
