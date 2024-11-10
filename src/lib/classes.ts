54ecf0bc885ac7da4de78388a95356010c30cddd
Sage Abdullah <me@laymonage.com>
2024-11-10 20:47:38 +0000

    Implement theme switcher

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -1,2 +1,2 @@
-export const prose = 'prose prose-lg sm:prose-xl';
+export const prose = 'prose prose-lg sm:prose-xl dark:prose-invert';
 export const proseList = 'prose-ol:list-none prose-ol:pl-0 prose-li:pl-0';
