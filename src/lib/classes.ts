d526dfda38fd8e9080eeca1eab1b9a10f0df2ba3
Sage Abdullah <me@laymonage.com>
2024-11-14 04:34:13 +0000

    Tweak spacing

--- src/lib/classes.ts
+++ src/lib/classes.ts
@@ -1,2 +1,3 @@
 export const prose = 'prose prose-lg sm:prose-xl dark:prose-invert';
-export const proseList = 'prose-ol:list-none prose-ol:pl-0 prose-li:pl-0';
+export const proseList =
+  'prose-ol:list-none prose-ol:pl-0 prose-li:pl-0 prose-h3:mt-0';
