c5bfefbb9ae47e43c32153731b625d1f5c86b94e
Sage Abdullah <me@laymonage.com>
2024-11-24 05:52:50 +0000

    Replace decodeHTML lib function with one from entities

--- src/lib/string.ts
+++
@@ -1,6 +0,0 @@
-export const decodeHTML = (html: string) => {
-  if (typeof document === 'undefined') return html;
-  const text = document.createElement('textarea');
-  text.innerHTML = html;
-  return text.value;
-};
